a148fbf549b2bb0faf9f258f39254633a9729607
Lucas Hill <kingofallhills@gmail.com>
2017-04-02 23:41:36 -0700

    Add test for blueprint.

--- blueprints/ember-print-this/index.js
+++ blueprints/ember-print-this/index.js
@@ -1,7 +1,11 @@
 /* eslint-env node */
 module.exports = {
   description: '',
-
+  
+  normalizeEntityName: function() {
+    //noop
+  },
+  
   afterInstall: function(options) {
     return this.addBowerPackageToProject('printThis', '1.9.0');
   }
