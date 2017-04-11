807d351b88cf769980a660d6f5080be1325cba2d
Lucas Hill <kingofallhills@gmail.com>
2017-04-10 23:58:37 -0700

    Add action usage of print and test.

--- addon/components/print-this.js
+++ addon/components/print-this.js
@@ -3,14 +3,25 @@ import layout from '../templates/components/print-this';
 
 export default Ember.Component.extend({
   layout,
-  printClass: 'content__printThis',
+  defaultPrintClass: 'content__printThis',
+  printClass: null,
   autoPrint: false,
-  
-  didInsertElement() {
-    if(this.get('autoPrint')) {
-      this.get('_jQuery')(`.${this.get('printClass')}`).printThis();
+  options: null, //pass in options hash for print this
+  actions: {
+    doPrint() {
+      this._print();
     }
   },
 
+  didInsertElement() {
+    if(this.get('autoPrint')) {
+      this._print();
+    }
+  },
+  
+  _print() {
+    const printClass = this.get('printClass') || this.get('defaultPrintClass');
+    this.get('_jQuery')(`.${printClass}`).printThis();
+  },
   _jQuery: Ember.$
 });
