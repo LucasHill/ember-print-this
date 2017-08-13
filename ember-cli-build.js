8e7a3a4ae6954017c86aa73351e235dcac84a853
Jonathan Johnson <jon.johnson@ucsf.edu>
2017-08-13 15:21:45 -0700

    Install print-this from NPM instead of bower
    This makes it easier to keep up to date and doesn't require bower for
    new apps where it isn't needed anymore.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -12,8 +12,5 @@ module.exports = function(defaults) {
     This build file does *not* influence how the addon or the app using it
     behave. You most likely want to be modifying `./index.js` or app's build file
   */
-
-  app.import('bower_components/printThis/printThis.js');
-  
   return app.toTree();
 };
--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 /* eslint-env node */
 'use strict';
 
+var path = require('path');
+var Funnel = require('broccoli-funnel');
+var MergeTrees = require('broccoli-merge-trees');
+
 module.exports = {
   name: 'ember-print-this',
 
-
-  included: function(app) {
+  included(app) {
     this._super.included.apply(this, arguments);
 
-    app.import(app.bowerDirectory + '/printThis/printThis.js');
-  }
+    if (typeof app.import !== 'function' && app.app) {
+      app = app.app;
+    }
+    app.import('vendor/printThis.js');
+  },
+
+  treeForVendor(vendorTree) {
+    var printThisTree = new Funnel(path.dirname(require.resolve('print-this/printThis.js')), {
+      files: ['printThis.js'],
+    });
+
+    return new MergeTrees([vendorTree, printThisTree]);
+  },
 };
