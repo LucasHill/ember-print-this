13993f1a89e313e0a3d5810829c01ffcce6c06a7
Lucas Hill <kingofallhills@gmail.com>
2017-08-14 00:39:14 -0700

    Include vendorTree if not undefined.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 
 var path = require('path');
 var Funnel = require('broccoli-funnel');
+var MergeTrees = require('broccoli-merge-trees');
 
 module.exports = {
   name: 'ember-print-this',
@@ -16,11 +17,11 @@ module.exports = {
     app.import('vendor/printThis.js');
   },
 
-  treeForVendor() {
+  treeForVendor(vendorTree) {
     var printThisTree = new Funnel(path.dirname(require.resolve('print-this/printThis.js')), {
       files: ['printThis.js'],
     });
 
-    return printThisTree;
+    return vendorTree ? new MergeTrees([vendorTree, printThisTree]): printThisTree;
   },
 };
